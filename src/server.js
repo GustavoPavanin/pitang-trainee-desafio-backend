b6776c06683df4d7268dc42d90e211fdd6d251a9
Gustavo Pavanin Martins <GustavoPavanin>
2022-04-21 19:33:12 -0300

    make it better

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ import detEnv from 'dotenv';
 
 detEnv.config();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server running on PORT ${PORT} ----- Aplicação Iniciada`);
